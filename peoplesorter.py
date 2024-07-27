1e5261e1bf38096c75d43867f44c41b789bcddf9
Dillus66 <powerdillon463@gmail.com>
2024-07-27 10:41:45 +0100

    test

---
+++ peoplesorter.py
@@ -0,0 +1,50 @@
+person1 = {
+    'name': 'matt',
+    'age': 30,
+    'sex': 'boy'
+}
+
+person2 = {
+    'name': 'george',
+    'age': 45,
+    'sex': 'boy'
+}
+
+person3 = {
+    'name': 'kara',
+    'age': 26,
+    'sex': 'girl'
+}
+
+person4 = {
+    'name': 'madonna',
+    'age': 40,
+    'sex': 'girl'
+}
+
+person5 = {
+    'name': 'jack',
+    'age': 60,
+    'sex': 'boy'
+}
+
+person6 = {
+    'name': 'max',
+    'age': 28,
+    'sex': 'boy'
+}
+
+person7 = {
+    'name': 'kass',
+    'age': 20,
+    'sex': 'girl'
+}
+
+person8 = {
+    'name': 'sousie',
+    'age': 34,
+    'sex': 'girl'
+}
+
+
+name = 'conor'
